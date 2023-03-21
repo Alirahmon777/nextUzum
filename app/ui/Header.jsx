4820b1b6c65eaac8d11e5417b20b113c76a7be22
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-03-21 11:16:26 +0500

    corrected-mistakes

--- app/ui/Header.jsx
+++ app/ui/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
