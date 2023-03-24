01db65eec520ac76031068e25fd8ba45eb6fd462
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-03-24 11:08:06 +0500

    finished-favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,24 +6,69 @@ import "./card.scss";
 import FavoriteContextProvider from "./contexts/isFavorite";
 import SetDataContextProvider from "./contexts/setDatas";
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
-
 export const metadata = {
   title: "Uzum Market — internet-doʻkon",
+  icons: {
+    icon: [
+      {
+        url: "/_next/static/media/metadata/favicon/favicon.ico",
+        type: "image/x-icon",
+        sizes: "any",
+      },
+    ],
+    maskIcon: "/_next/static/media/metadata/favicon/safari-pinned-tab.svg",
+    shortcut: "/_next/static/media/metadata/favicon/shortcut-icon.png",
+    other: [
+      {
+        rel: "apple-touch-icon",
+        url: "/_next/static/media/metadata/favicon/apple-touch-icon.png",
+        sizes: "120x120",
+      },
+      {
+        rel: "mask-icon",
+        url: "/_next/static/media/metadata/favicon/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+      {
+        rel: "icon",
+        url: "/_next/static/media/metadata/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/_next/static/media/metadata/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+  },
+
+  manifest: "/_next/static/media/metadata/favicon/site.webmanifest",
   description:
     "Katta miqdorda chegirmali mahsulotlar: elektronika, maishiy texnika, bolalar tovarlari, kiyim-kechak va poyabzal, uy, sport, goʻzallik va sogʻliq uchun hamma mahsulotlar, kitoblar, uy hayvonlari uchun tovarlar va boshqalar. 12 oylik muddatli toʻlov. 1 kunda yetkazib berish. Uzum Marketda har kuni chegirmalar va aktsiyalar!",
 };
+{
+  //  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+  // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  // <link rel="manifest" href="/site.webmanifest">
+  // <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  // <meta name="msapplication-TileColor" content="#da532c">
+  // <meta name="theme-color" content="#ffffff"></meta>
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <FavoriteContextProvider>
-        <SetDataContextProvider>
+    <FavoriteContextProvider>
+      <SetDataContextProvider>
+        <html lang="en">
           <body className={roboto.className}>
             <Header />
             {children}
           </body>
-        </SetDataContextProvider>
-      </FavoriteContextProvider>
-    </html>
+        </html>
+      </SetDataContextProvider>
+    </FavoriteContextProvider>
   );
 }
--- app/wishes/page.jsx
+++ app/wishes/page.jsx
@@ -2,8 +2,6 @@ import FavoriteProducts from "./Favourite";
 export const metadata = {
   title:
     "Uzum - mahsulotlari kunning ertasiga yetkazib beriladigan ilk Oʻzbekiston savdo maydoni",
-  description:
-    "Katta miqdorda chegirmali mahsulotlar: elektronika, maishiy texnika, bolalar tovarlari, kiyim-kechak va poyabzal, uy, sport, goʻzallik va sogʻliq uchun hamma mahsulotlar, kitoblar, uy hayvonlari uchun tovarlar va boshqalar. 12 oylik muddatli toʻlov. 1 kunda yetkazib berish. Uzum Marketda har kuni chegirmalar va aktsiyalar!",
 };
 
 const page = () => {
@@ -17,6 +15,5 @@ const page = () => {
     </section>
   );
 };
-
 
 export default page;
